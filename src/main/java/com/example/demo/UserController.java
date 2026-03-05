084903ecd6c90fa92b4dda634e20bd68b9870b8d
harshitha rayudu <harshitharayudu06@gmail.com>
2026-03-05 11:52:26 +0530

    backend update

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@CrossOrigin(origins= "*")
+@CrossOrigin(origins = "https://demo-front-xi.vercel.app")
 
 public class UserController {
     @Autowired
